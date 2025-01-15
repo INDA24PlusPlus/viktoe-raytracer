e94e32c6ecf06dd50f147ea85396b7ddd35b565a
WALL-E <wall-e.axiom@proton.me>
2025-01-15 17:36:51 +0100

    ref: move image logic to main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 use std::fs::File;
 
-mod ppm;
+use color::Color;
+
 mod color;
+mod ppm;
 
 fn main() {
     let mut file = File::create("image.ppm").unwrap();
     let ppm = ppm::PPMImage::default();
 
-    ppm.print(&mut file);
+    let mut image = Vec::new();
+
+    for height in 0..ppm.height {
+        println!("Scanlines remaining: {}", ppm.height - height);
+        for width in 0..ppm.width {
+            let color = Color::new(
+                (width as f64 / (ppm.width - 1) as f64 * 256.0) as u8,
+                (height as f64 / (ppm.height) as f64 * 256.0) as u8,
+                0,
+            );
+            image.push(color);
+        }
+    }
+    println!("Done");
+    ppm.print(&mut file, image);
 }
--- src/ppm.rs
+++ src/ppm.rs
@@ -3,40 +3,29 @@ use std::{fs::File, io::Write};
 use crate::color::Color;
 
 pub struct PPMImage {
-    width: usize,
-    height: usize,
-    color_range: usize,
+    pub width: usize,
+    pub height: usize,
 }
 
 impl Default for PPMImage {
     fn default() -> Self {
-        Self::new(256, 256, 255)
+        Self::new(256, 256)
     }
 }
 
 impl PPMImage {
-    fn new(width: usize, height: usize, color_range: usize) -> Self {
-        PPMImage {
-            width,
-            height,
-            color_range,
-        }
+    fn new(width: usize, height: usize) -> Self {
+        PPMImage { width, height }
     }
 
-    pub fn print(&self, file: &mut File) {
+    pub fn print(&self, file: &mut File, image: Vec<Color>) {
         file.write_all(b"P3\n").unwrap();
         file.write_fmt(format_args!("{} {}\n", self.width, self.height))
             .unwrap();
-        file.write_fmt(format_args!("{}\n", self.color_range))
-            .unwrap();
+        file.write_fmt(format_args!("{}\n", 255)).unwrap();
 
-        for height in 0..self.height {
-            println!("Scanlines remaining: {}", self.height - height);
-            for width in 0..self.width {
-                let color = Color::new((width as f64 / (self.width - 1) as f64 * 256.0) as u8, (height as f64 / (self.height) as f64 * 256.0) as u8, 0);
-                color.write(file);
-            }
+        for pixel in image {
+            pixel.write(file);
         }
-        println!("Done");
     }
 }
